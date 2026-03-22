9dc89b663a3e676e0fe9eac88d0a5d641cf59754
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-22 17:25:53 +0530

    Install gsap and create basic animation text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import gsap from 'gsap';
+import './App.css';
+import { useEffect ,useRef } from 'react';
 
 function App() {
+
+  const boxRef = useRef();
+
+  useEffect(() => {
+    gsap.fromTo(
+      boxRef.current,
+      {
+        y: 100,
+        opacity: 0,
+        scale: 0.5,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        scale: 1,
+        duration: 3,
+        ease: "power3.out",
+      }
+    );
+  }, []);
  
   return (
-   <div>
-    <h1 className='text-4xl font-bold'>OK</h1>
+   <div className='h-screen flex items-center justify-center'>
+    <div ref={boxRef} className="w-20 h-20 bg-black"></div>
+    
    </div>
   )
 }
