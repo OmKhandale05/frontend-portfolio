69b0f95a52254900d4062694187b6df6c253cd3a
OmPrakash05 <omprakashkhandale5@gmail.com>
2026-03-22 17:41:56 +0530

    Create loader component and basic lyout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
-import gsap from 'gsap';
 import './App.css';
-import { useEffect ,useRef } from 'react';
+import Loader from './components/Loader';
 
 function App() {
 
-  const boxRef = useRef();
 
-  useEffect(() => {
-    gsap.fromTo(
-      boxRef.current,
-      {
-        y: 100,
-        opacity: 0,
-        scale: 0.5,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        scale: 1,
-        duration: 3,
-        ease: "power3.out",
-      }
-    );
-  }, []);
- 
-  return (
-   <div className='h-screen flex items-center justify-center'>
-    <div ref={boxRef} className="w-20 h-20 bg-black"></div>
-    
-   </div>
-  )
+  return <Loader/>;
 }
 
 export default App
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return ( 
+        <div className="fixed inset-0 bg-black text-white flex items-center justify-center">
+            <h1 className="text-5xl">Loading...</h1>
+        </div>
+     );
+}
+ 
+export default Loader;
